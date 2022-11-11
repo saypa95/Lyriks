8e0983f202838768f133d06ff5a2e0d9c6035ec8
saypa95 <saypa_95@mail.ru>
2022-11-11 12:36:32 +0300

    first commit

---
+++ src/redux/services/shazamCore.js
@@ -0,0 +1,34 @@
+import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
+
+const options = {
+  method: "GET",
+  headers: {
+    "X-RapidAPI-Key": "94a57381f4msh2c69735f03071b9p1af559jsn2643dd3c034f",
+    "X-RapidAPI-Host": "shazam-core.p.rapidapi.com",
+  },
+};
+
+export const shazamCoreApi = createApi({
+  reducerPath: "shazamCoreApi",
+  baseQuery: fetchBaseQuery({
+    baseUrl: "https://shazam-core.p.rapidapi.com/v1",
+    prepareHeaders: (headers) => {
+      headers.set("X-RapidAPI-Key", "94a57381f4msh2c69735f03071b9p1af559jsn2643dd3c034f");
+
+      return headers;
+    },
+  }),
+  endpoints: (builder) => ({
+    getTopCharts: builder.query({
+      query: () => "/charts/world",
+    }),
+    getSongDetails: builder.query({
+      query: (songid) => `/tracks/details?track_id=${songid}`,
+    }),
+    getSongRelated: builder.query({
+      query: ({ songid }) => `/tracks/related?track_id=${songid}`,
+    }),
+  }),
+});
+
+export const { useGetTopChartsQuery, useGetSongDetailsQuery, useGetSongRelatedQuery } = shazamCoreApi;
