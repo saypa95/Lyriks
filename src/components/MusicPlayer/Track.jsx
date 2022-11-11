0759be7062a1320795d8cdc32ed891f68ee6edd6
saypa95 <saypa_95@mail.ru>
2022-11-11 18:47:32 +0300

    a little fixes

--- src/components/MusicPlayer/Track.jsx
+++ src/components/MusicPlayer/Track.jsx
@@ -5,9 +5,9 @@ const Track = ({ isPlaying, isActive, activeSong }) => (
     <div className={`${isPlaying && isActive ? 'animate-[spin_3s_linear_infinite]' : ''} hidden sm:block h-16 w-16 mr-4`}>
       <img src={activeSong?.images?.coverart} alt="cover art" className="rounded-full" />
     </div>
-    <div className="w-[50%]">
+    <div className="w-[200px]">
       <p className="truncate text-white font-bold text-lg">
-        {activeSong?.title ? activeSong?.title.slice(0, 30) : 'No active Song'}
+        {activeSong?.title ? activeSong?.title : 'No active Song'}
       </p>
       <p className="truncate text-gray-300">
         {activeSong?.subtitle ? activeSong?.subtitle : 'No active Song'}
